ab11ec635a41cc21ab9a0d048ebf9fcbe42ee199
Jon Sherrard <jon@andthats.it>
2012-03-06 17:06:50 +0000

    First Upload of CI_Template
    Pre Configured CodeIgniter framework, with View Templating, Zurb
    Foundation CSS Framework and CI Sparks with ion auth.

---
+++ application/views/template.php
@@ -0,0 +1,9 @@
+<?php 
+
+$this->load->view('templates/header');
+
+$this->load->view($page);
+
+$this->load->view('templates/footer');
+
+?>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,19 @@
+	</div>
+	<!-- container -->
+
+
+
+
+	<!-- Included JS Files -->
+	<script src="javascripts/jquery.min.js"></script>
+	<!-- Combine and Compress These JS Files -->
+	<script src="javascripts/jquery.reveal.js"></script>
+	<script src="javascripts/jquery.orbit-1.4.0.js"></script>
+	<script src="javascripts/jquery.customforms.js"></script>
+	<script src="javascripts/jquery.placeholder.min.js"></script>
+	<script src="javascripts/jquery.tooltips.js"></script>
+	<!-- End Combine and Compress These JS Files -->
+	<script src="javascripts/app.js"></script>
+
+</body>
+</html>
